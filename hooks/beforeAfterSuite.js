3f79e1ccfbf92f72b57baddee21fed26dfbf0f9b
Anantha Rajan, Akhil Srinivas <AkhilSrinivas.AnanthaRajan@hmhco.com>
2024-09-03 10:23:44 +0100

    update bootstrap

--- hooks/beforeAfterSuite.js
+++ hooks/beforeAfterSuite.js
@@ -4,38 +4,17 @@ const path = require('path');
 
 module.exports = {
   bootstrap: async () => {
-    console.log('Cleaning up reports (Only the latest 3 will be available for viewing if exists)');;
+    console.log('Deleting the reports directory...');
 
     const reportsDir = path.join(__dirname, '../reports');
 
-    // Get a list of all files in the reports directory
-    fs.readdir(reportsDir, (err, files) => {
+    // Delete the reports directory and all its contents
+    fs.rm(reportsDir, { recursive: true, force: true }, (err) => {
       if (err) {
-        return console.error(`Unable to scan directory: ${err}`);
+        console.error(`Failed to delete directory: ${reportsDir} - ${err}`);
+      } else {
+        console.log(`Deleted directory: ${reportsDir}`);
       }
-
-      // Sort files by their creation time (newest first)
-      const sortedFiles = files
-        .map(file => ({
-          name: file,
-          time: fs.statSync(path.join(reportsDir, file)).mtime.getTime()
-        }))
-        .sort((a, b) => b.time - a.time)
-        .map(file => file.name);
-
-      // Keep only the latest 3 files, delete the rest
-      const filesToDelete = sortedFiles.slice(3);
-
-      filesToDelete.forEach(file => {
-        const filePath = path.join(reportsDir, file);
-        fs.unlink(filePath, err => {
-          if (err) {
-            console.error(`Failed to delete file: ${filePath} - ${err}`);
-          } else {
-            console.log(`Deleted file: ${filePath}`);
-          }
-        });
-      });
     });
   },
 
